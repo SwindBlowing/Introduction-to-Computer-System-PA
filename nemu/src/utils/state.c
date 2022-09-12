0decab4982dddd5bbec7b393f2805860a928e9ac
tracer-ics2022 <tracer@njuics.org>
2022-09-12 15:05:10 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:05:10 up  3:19,  1 user,  load average: 0.45, 0.29, 0.27

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -19,7 +19,7 @@ NEMUState nemu_state = { .state = NEMU_STOP };
 
 int is_exit_status_bad() {
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
-    (nemu_state.state == NEMU_QUIT);
-  Log("%d", nemu_state.state);
+    (nemu_state.state == NEMU_QUIT) || (nemu_state.state == NEMU_STOP);
+  //Log("%d", nemu_state.state);
   return !good;
 }
