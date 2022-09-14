c2d858a63f5272d1c76de45feeecc3bb810ccc28
tracer-ics2022 <tracer@njuics.org>
2022-09-14 14:00:46 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:00:46 up  1:27,  1 user,  load average: 0.85, 0.39, 0.27

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -136,6 +136,10 @@ static int cmd_si(char *args) {
 
 static int cmd_info(char *args) {
   char *arg = strtok(NULL, " ");
+  if (arg == NULL || strlen(arg) > 1) {
+    printf("Unknown command '%s'\n", arg);
+    return 0;
+  }
   switch (*arg)
   {
     case 'f':
