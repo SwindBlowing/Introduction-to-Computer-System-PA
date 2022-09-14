323b5e3afc7b3ee50c3a17874853b33fce131fa0
tracer-ics2022 <tracer@njuics.org>
2022-09-14 16:09:51 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:09:51 up  3:36,  1 user,  load average: 0.21, 0.23, 0.27

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -219,9 +219,10 @@ static int cmd_p(char *args)
 static int cmd_w(char *args)
 {
   WP *now = new_wp();
-  assert(now != NULL);
+  //assert(now != NULL);
   if (now != NULL) {
     now->args = args;
+    printf("%s\n", now->args);
     bool success = 1;
     now->pre_val = expr(args, &success);
     if (!success) now->Divided0 = 1;
