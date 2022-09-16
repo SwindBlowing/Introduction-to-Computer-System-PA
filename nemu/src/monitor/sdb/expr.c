aa4a50591471e64ad2e0fd312a8a0afdb4069159
tracer-ics2022 <tracer@njuics.org>
2022-09-16 22:33:13 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:33:13 up 26 min,  1 user,  load average: 0.18, 0.11, 0.17

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -309,7 +309,7 @@ word_t expr(char *e, bool *success) {
   /* TODO: Insert codes to evaluate the expression. */
   //TODO();
 
-  //for (int i = 0; i < nr_token; i++)
-  //  printf("%s\n", tokens[i].str);
+  for (int i = 0; i < nr_token; i++)
+    printf("%d\n", tokens[i].type);
   return eval(0, nr_token - 1, success);
 }
