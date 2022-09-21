69eca909c50b907c9f9737ff47a24c30c60b19f5
tracer-ics2022 <tracer@njuics.org>
2022-09-21 10:00:38 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:00:38 up 17 min,  1 user,  load average: 0.75, 0.55, 0.53

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,19 +5,50 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t len = 0;
+  while (*(s + len) != '\0') 
+    len++;
+  return len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t len = 0;
+  while (*(src + len) != '\0') {
+    *(dst + len) = *(src + len);
+    len++;
+  }
+  *(dst + len) = *(src + len);
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t len = 0;
+  while (*(src + len) != '\0' && len < n) {
+    *(dst + len) = *(src + len);
+    len++;
+  }
+  if (len < n) {
+    while (len < n) {
+      *(dst + len) = '\0';
+      len++;
+    }
+  }
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t len1 = 0, len2 = 0;
+  while (*(dst + len1) != '\0') len1++;
+  while (*(src + len2) != '\0') {
+    *(dst + len1 + len2) = *(src + len2);
+    len2++;
+  }
+  *(dst + len1 + len2) = '\0';
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
