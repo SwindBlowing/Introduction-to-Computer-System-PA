4ae0f587afd192d8d60d2aede938ca6a4eaaedb8
tracer-ics2022 <tracer@njuics.org>
2022-09-21 11:37:02 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:37:02 up  1:54,  1 user,  load average: 1.11, 0.69, 0.62

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,50 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  va_list ap;
+  int len = 0;
+  int d = 0;
+  long long ld = 0, lf = 1;
+  char *s = NULL;
+  va_start(ap, fmt);
+  while (*fmt != '\0') {
+    if (*fmt == '%') {
+      fmt++;
+      switch(*fmt) {
+        case 's' :
+          s = va_arg(ap, char *);
+          strcat(out, s);
+          len = strlen(out);
+          break;
+        case 'd' :
+          d = va_arg(ap, int);
+          ld = d;
+          lf = 1;
+          if (ld < 0) {
+            ld *= -1;
+            *(out + len) = '-';
+            len++;
+          }
+          while (lf * 10 <= ld) lf *= 10;
+          while (lf) {
+            *(out + len) = '0' + ld / lf;
+            len++;
+            ld %= lf;
+            lf /= 10;
+          }
+          break;
+        default : return -1;
+      }
+    }
+    else {
+      *(out + len) = *fmt;
+      len++;
+    }
+    fmt++;
+  }
+  va_end(ap);
+  return strlen(out);
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
