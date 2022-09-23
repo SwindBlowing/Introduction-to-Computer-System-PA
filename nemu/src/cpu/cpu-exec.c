88f96b5aba3e751a92f924ca8eb59a3944ac3066
tracer-ics2022 <tracer@njuics.org>
2022-09-23 23:21:51 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:21:51 up 34 min,  1 user,  load average: 0.19, 0.61, 0.56

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -150,6 +150,6 @@ void cpu_exec(uint64_t n) {
             ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           nemu_state.halt_pc);
       // fall through
-    case NEMU_QUIT: statistic();
+    case NEMU_QUIT: iringbufPrint(), statistic();
   }
 }
