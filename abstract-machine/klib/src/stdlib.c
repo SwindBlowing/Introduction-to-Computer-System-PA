3dd1a27b0905a91e9ca9becca5d3b6bf7074bf6e
tracer-ics2022 <tracer@njuics.org>
2022-09-26 21:18:45 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:18:45 up  1:02,  1 user,  load average: 0.68, 0.41, 0.26

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -33,10 +33,13 @@ void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+/*#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
   panic("Not implemented");
-#endif
-  return NULL;
+#endif*/
+  if (!size) return NULL;
+  char *old = heap.start;
+  heap.start += size;
+  return old;
 }
 
 void free(void *ptr) {
