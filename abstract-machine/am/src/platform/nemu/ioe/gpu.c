2507f98fdc6f35956b88a031aaf299e12b971eeb
tracer-ics2022 <tracer@njuics.org>
2022-09-27 12:26:00 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:26:00 up  1:45,  1 user,  load average: 0.57, 0.35, 0.21

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,8 +5,8 @@
 
 void __am_gpu_init() {
   int i;
-  int w = (inl(VGACTL_ADDR) >> 16) / 32;  // TODO: get the correct width
-  int h = (uint16_t)inl(VGACTL_ADDR) / 32;  // TODO: get the correct height
+  int w = (inl(VGACTL_ADDR) >> 16);  // TODO: get the correct width
+  int h = (uint16_t)inl(VGACTL_ADDR);  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
