8e856d7fadf550e8838625ce5f4b997bf8f0e708
tracer-ics2022 <tracer@njuics.org>
2022-09-27 14:05:15 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:05:15 up 25 min,  1 user,  load average: 0.52, 0.55, 0.45

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,8 +5,8 @@
 
 void __am_gpu_init() {
   int i;
-  int w = (inl(VGACTL_ADDR) >> 16);  // TODO: get the correct width
-  int h = (uint16_t)inl(VGACTL_ADDR);  // TODO: get the correct height
+  int w = (inl(VGACTL_ADDR) >> 16) * 32;  // TODO: get the correct width
+  int h = (uint32_t)((uint16_t)inl(VGACTL_ADDR)) * 32; // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
@@ -15,17 +15,17 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = (inl(VGACTL_ADDR) >> 16), .height = (uint16_t)inl(VGACTL_ADDR),
+    .width = (inl(VGACTL_ADDR) >> 16) * 32, .height = (uint32_t)((uint16_t)inl(VGACTL_ADDR)) * 32,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  int W = (inl(VGACTL_ADDR) >> 16);
+  /*int W = (inl(VGACTL_ADDR) >> 16);
   for (int i = 0; i < ctl->h; i++) 
     for (int j = 0; j < ctl->w; j++){
       outl(FB_ADDR + W * (ctl->x + i) + ctl->y + j, *((uint32_t *)(ctl->pixels) + i * (ctl->w) + j));
-    }
+    }*/
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
