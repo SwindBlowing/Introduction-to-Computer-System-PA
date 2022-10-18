c85028f8711f079947a43dd56473ed65d392b5fa
tracer-ics2022 <tracer@njuics.org>
2022-10-18 14:21:51 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:21:51 up 39 min,  1 user,  load average: 0.71, 0.46, 0.32

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -45,6 +45,8 @@ void sdb_set_batch_mode();
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
+static char *elf_file = NULL;
+
 static int difftest_port = 1234;
 
 static long load_img() {
@@ -76,11 +78,13 @@ static int parse_args(int argc, char *argv[]) {
     {"diff"     , required_argument, NULL, 'd'},
     {"port"     , required_argument, NULL, 'p'},
     {"help"     , no_argument      , NULL, 'h'},
+	{"elf"      , required_argument, NULL, 'e'},
     {0          , 0                , NULL,  0 },
   };
   int o;
   while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
     switch (o) {
+	  case 'e': elf_file = optarg; break;
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
