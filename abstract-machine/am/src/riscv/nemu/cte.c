18f1b6c557963b6b27e1195a58c3f7355e2ee7a1
tracer-ics2022 <tracer@njuics.org>
2022-10-19 21:02:03 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:02:03 up  2:43,  1 user,  load average: 0.25, 0.36, 0.33

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,17 +5,17 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
+  printf("%lu %lu %lu\n", c->mcause, c->mepc, c->mstatus);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
       default: ev.event = EVENT_ERROR; break;
     }
-	
-    c = user_handler(ev, c);assert(0);
+    c = user_handler(ev, c);
     assert(c != NULL);
   }
   
-  printf("%lu %lu %lu\n", c->mcause, c->mepc, c->mstatus);
+  
   return c;
 }
 
