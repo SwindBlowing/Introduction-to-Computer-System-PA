848258ab7ff56a3f36a36a32308ad180ddbf55a9
tracer-ics2022 <tracer@njuics.org>
2022-10-19 15:07:19 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:07:19 up 46 min,  1 user,  load average: 0.62, 0.28, 0.20

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -65,11 +65,8 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   });
 
 #ifdef CONFIG_WATCHPOINT
-  /*if (!wp_pause()) {
-    //printf("arrived!");
-    //set_nemu_state(NEMU_STOP, cpu.pc, )
-    nemu_state.state = NEMU_STOP;
-  }*/
+ // if (!wp_pause()) 
+  //  nemu_state.state = NEMU_STOP;
 #endif
     
 }
