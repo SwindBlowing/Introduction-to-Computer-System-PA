f2b896735136332891ece1eec781effdd061d9f2
tracer-ics2022 <tracer@njuics.org>
2022-10-19 20:05:23 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:05:23 up  1:46,  1 user,  load average: 0.20, 0.28, 0.26

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -31,7 +31,7 @@ static char ins[IRINGBUF_NUM][100];
 static int nowcnt = 0;
 #endif
 
-CPU_state cpu = {};
+CPU_state cpu = {.mstatus = 0x1800,};
 uint64_t g_nr_guest_inst = 0;
 static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;
