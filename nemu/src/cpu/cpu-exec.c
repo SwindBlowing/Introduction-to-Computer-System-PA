f57a788406dc0edb095f744807744b2152f79114
tracer-ics2022 <tracer@njuics.org>
2022-10-19 20:25:18 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:25:18 up  2:06,  1 user,  load average: 0.04, 0.14, 0.20

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -56,13 +56,14 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
 #endif
-  if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
-  IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
-  IFDEF(CONFIG_IRINGBUF, {
+IFDEF(CONFIG_IRINGBUF, {
     strcpy(ins[nowcnt], _this->logbuf);
     nowcnt = (nowcnt + 1) % IRINGBUF_NUM;
   });
+
+  if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
+  IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
 #ifdef CONFIG_WATCHPOINT
   if (!wp_pause()) 
