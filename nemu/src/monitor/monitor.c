b8d5914b3476bf5aff3c03c51b42dc25b8d053e2
tracer-ics2022 <tracer@njuics.org>
2022-10-19 10:04:38 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:04:38 up 20 min,  1 user,  load average: 0.75, 0.58, 0.47

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -57,6 +57,7 @@ static void load_elf()
 	Elf32_Ehdr ehdr;
 	Elf32_Shdr shdrs[99];
 	Elf32_Sym symtabs[999];
+	int num_of_sym = 0;
 	bool p = fread(&ehdr, sizeof(Elf32_Ehdr), 1, fp); p = 1; assert(p);
 	fseek(fp, ehdr.e_shoff, SEEK_SET);
 	p = fread(shdrs, sizeof(Elf32_Shdr), ehdr.e_shnum, fp); p = 1; assert(p);
@@ -65,11 +66,13 @@ static void load_elf()
 		fseek(fp, shdrs[ehdr.e_shstrndx].sh_offset + shdrs[i].sh_name, SEEK_SET);
 		p = fread(ch, sizeof(char *), 1, fp); p = 1; assert(p);
 		if (strcmp(ch, ".symtab") == 0) {
+			num_of_sym = shdrs[i].sh_size * 8 / sizeof(Elf32_Sym);
 			fseek(fp, shdrs[i].sh_offset, SEEK_SET);
-			p = fread(&symtabs[1], sizeof(Elf32_Sym), 1, fp); p = 1; assert(p);
+			p = fread(symtabs, sizeof(Elf32_Sym), num_of_sym, fp); p = 1; assert(p);
 		}
 	}
-	printf("%d", symtabs[1].st_name);
+	for (int i = 0; i < num_of_sym; i++)
+		printf("0x%x\n", symtabs[i].st_name);
 	fclose(fp);
 }
 
