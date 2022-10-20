55f5b6c0bc78869b66552164214e3f5f8eea733b
tracer-ics2022 <tracer@njuics.org>
2022-10-20 14:12:26 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:12:26 up 19 min,  1 user,  load average: 0.96, 0.59, 0.44

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -10,6 +10,8 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
+	  case 0x0000000b:
+	  	ev.event = EVENT_YIELD; break;
       default: ev.event = EVENT_ERROR; break;
     }
     c = user_handler(ev, c);
--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,6 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
+	case EVENT_YIELD: panic("Received yield message!");
     default: panic("Unhandled event ID = %d", e.event);
   }
 
