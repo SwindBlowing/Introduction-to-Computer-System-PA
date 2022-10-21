ad4ac47dab34e727da4e26571e26f0ef53721402
tracer-ics2022 <tracer@njuics.org>
2022-10-21 20:22:32 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:22:32 up  3:13,  1 user,  load average: 0.17, 0.50, 0.41

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -33,10 +33,6 @@ Context* __am_irq_handle(Context *c) {
 			case 0x12:
 			case 0x13:
 				ev.event = EVENT_SYSCALL; 
-				#ifdef CONFIG_STRACE
-  					printf("System call ID = %u\n", c->GPR1);
-  					printf("GPRS value: %x %x %x %x %x\n", c->GPR1, c->GPR2, c->GPR3, c->GPR4, c->GPRx);
-				#endif
 				break;
 			case 0xffffffff:
 				ev.event = EVENT_YIELD; break;
