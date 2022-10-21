5c5a27ee2ab2e7919419a6cf40665eb301ff2562
tracer-ics2022 <tracer@njuics.org>
2022-10-21 13:54:09 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:54:09 up 21 min,  1 user,  load average: 0.29, 0.27, 0.24

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,14 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
-  //FILE *fp = fopen("../build/nanos-lite-riscv32-nemu.elf", "rb");
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+
   return 0;
 }
 
