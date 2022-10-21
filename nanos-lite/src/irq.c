1f2e19fe39505ffdf15180f3f7ea424447987cfc
tracer-ics2022 <tracer@njuics.org>
2022-10-21 16:13:39 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:13:39 up  2:40,  1 user,  load average: 0.44, 0.48, 0.38

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
 	case EVENT_YIELD: printf("Received yield message!\n"); 
-		printf("Event ID = %d\n", e.event); break;
+		printf("%x %x %x\n", c->mstatus, c->mepc, c->mcause); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
