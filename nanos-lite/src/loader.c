fad6f2eab5d8b0caefde0c5979cc64b0e5d16a9e
tracer-ics2022 <tracer@njuics.org>
2022-10-21 16:03:32 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:03:32 up  2:30,  1 user,  load average: 0.55, 0.42, 0.31

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -29,7 +29,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 		size_t memSize = phdr[i].p_memsz;
 		printf("%x %x %x %x\n", offset, virtAddr, fileSize, memSize);
 		ramdisk_read((void *)virtAddr, offset, fileSize);
-		memset((void *)(virtAddr + fileSize), 0, memSize - fileSize);
+		if (memSize > fileSize) memset((void *)(virtAddr + fileSize), 0, memSize - fileSize);
 	}
   }
   return ehdr.e_entry;
