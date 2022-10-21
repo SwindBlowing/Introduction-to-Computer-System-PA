472f0ff29b24c74f34d641ece8faf51ef0800e84
tracer-ics2022 <tracer@njuics.org>
2022-10-21 20:06:55 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:06:55 up  2:57,  1 user,  load average: 0.65, 0.40, 0.30

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,14 +3,16 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-#ifdef CONFIG_STRACE
-  printf("System call ID = %u\n", a[0]);
-  printf("GPRS value: %x %x %x %x %x\n", c->GPR1, c->GPR2, c->GPR3, c->GPR4, c->GPRx);
-#endif
   switch (a[0]) {
 	case SYS_exit: halt(c->GPR2); break;
 	case SYS_yield: yield(); c->GPRx = 0; break;
     default: panic("Unhandled syscall ID = %u", a[0]);
   }
+  printf("arrived!\n");
+#ifdef CONFIG_STRACE
+assert(0);
+  printf("System call ID = %u\n", a[0]);
+  printf("GPRS value: %x %x %x %x %x\n", c->GPR1, c->GPR2, c->GPR3, c->GPR4, c->GPRx);
+#endif
 
 }
