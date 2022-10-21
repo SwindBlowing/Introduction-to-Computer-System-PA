2c848fe030987c7b33cde81116a700f058659d5c
tracer-ics2022 <tracer@njuics.org>
2022-10-21 18:42:24 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:42:24 up  1:33,  1 user,  load average: 0.20, 0.33, 0.35

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,6 @@ void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
 	case EVENT_SYSCALL: do_syscall(c); break;
-	case EVENT_YIELD: printf("Received yield message!\n"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,7 +3,6 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  printf("arrived!\n");
   switch (a[0]) {
 	case SYS_yield: yield(); c->GPRx = 1; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
