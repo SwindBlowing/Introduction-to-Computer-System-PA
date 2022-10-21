ca24ef6587d77cf3ab21d6e9464ab4dffd23f109
tracer-ics2022 <tracer@njuics.org>
2022-10-21 18:35:59 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:35:59 up  1:26,  1 user,  load average: 0.19, 0.39, 0.35

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,7 +3,7 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  printf("arrived!\n");
   switch (a[0]) {
 	case SYS_yield: yield(); c->GPRx = 1; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
