f61ee4c3f687843b96bb084b33f27a24af6c4c88
tracer-ics2022 <tracer@njuics.org>
2022-10-22 15:39:13 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:39:13 up 47 min,  1 user,  load average: 0.20, 0.17, 0.17

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -4,6 +4,7 @@ typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
 typedef struct {
   char *name;
@@ -12,6 +13,8 @@ typedef struct {
   ReadFn read;
   WriteFn write;
 } Finfo;
+
+static size_t open_offset[10005] = {-1};
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
 
@@ -41,16 +44,34 @@ int fs_open(const char *pathname, int flags, int mode)
 {
 	printf("size: %u\n", file_table->size);
 	for (int i = 0; i < file_table->size; i++)
-		if (strcmp(pathname, file_table[i].name) == 0)
+		if (strcmp(pathname, file_table[i].name) == 0) {
+			if (open_offset[i] == -1) open_offset[i] = file_table[i].disk_offset;
 			return i;
+		}
 	panic("No file found");
 	return -1;
 }
 size_t fs_read(int fd, void *buf, size_t len)
 {
-	ramdisk_read(buf, file_table[fd].disk_offset, len);
-	return 0;
+	len = len < file_table[fd].disk_offset + file_table[fd].size - open_offset[fd] ?
+			len : file_table[fd].disk_offset + file_table[fd].size - open_offset[fd];
+	ramdisk_read(buf, open_offset[fd], len);
+	fs_lseek(fd, len, SEEK_CUR);
+	return len;
 }
-size_t fs_write(int fd, const void *buf, size_t len);
-size_t fs_lseek(int fd, size_t offset, int whence);
-int fs_close(int fd);
+size_t fs_write(int fd, const void *buf, size_t len)
+{
+	len = len < file_table[fd].disk_offset + file_table[fd].size - open_offset[fd] ?
+			len : file_table[fd].disk_offset + file_table[fd].size - open_offset[fd];
+	ramdisk_write(buf, open_offset[fd], len);
+	fs_lseek(fd, len, SEEK_CUR);
+	return len;
+}
+size_t fs_lseek(int fd, size_t offset, int whence)
+{
+	return -1;
+}
+int fs_close(int fd)
+{
+	return -1;
+}
