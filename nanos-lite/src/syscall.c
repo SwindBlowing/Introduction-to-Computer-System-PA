78770f58d7ee3e268e84ce8b65a1a8b708012137
tracer-ics2022 <tracer@njuics.org>
2022-10-22 20:19:25 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:19:25 up  1:08,  1 user,  load average: 0.35, 0.46, 0.42

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -33,11 +33,13 @@ void do_syscall(Context *c) {
 	case SYS_brk: c->GPRx = 0; break;
 	case SYS_gettimeofday:
 		if ((void *)a[1] != NULL) {
+			printf("used %u\n", a[1]);
 			printf("arrived! %lu\n", io_read(AM_TIMER_UPTIME).us);
 			*(size_t *)(a[1]) = io_read(AM_TIMER_UPTIME).us / 1000000;
 			*(size_t *)(a[1] + sizeof(size_t)) = io_read(AM_TIMER_UPTIME).us;
 			printf("%ld\n", *(size_t *)(a[1]));
 			printf("%ld\n", *(size_t *)(a[1] + sizeof(size_t)));
+			printf("after %u\n", a[1]);
 		}
 		c->GPRx = 0;
 		break;
