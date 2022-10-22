856a2c9e79c529f28fe3af3b273d4f4d3023678d
tracer-ics2022 <tracer@njuics.org>
2022-10-22 12:55:42 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:55:42 up  1:32,  1 user,  load average: 0.08, 0.23, 0.24

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -16,7 +16,7 @@ void do_syscall(Context *c) {
 	case SYS_exit: halt(c->GPR2); break;
 	case SYS_yield: yield(); c->GPRx = 0; break;
 	case SYS_write: {
-		if (c->GPR2 == 1 || c->GPR2 == 2) {
+		if (c->GPR2 == 2) {
 			for (int i = 0; i < c->GPR4; i++)
 				putch(*(char *)(c->GPR3 + i));
 			c->GPRx = c->GPR4;
