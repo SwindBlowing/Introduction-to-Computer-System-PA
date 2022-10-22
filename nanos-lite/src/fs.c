d8e99789a5ac160604ed5d99b8bf8717408d48bb
tracer-ics2022 <tracer@njuics.org>
2022-10-22 17:40:23 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:40:23 up  2:48,  1 user,  load average: 0.23, 0.40, 0.45

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -29,11 +29,18 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
   return 0;
 }
 
+size_t system_write(const void *buf, size_t offset, size_t len)
+{
+	for (int i = 0; i < len; i++)
+		putch(*(char *)(buf + i));
+	return len;
+}
+
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, system_write},
+  [FD_STDERR] = {"stderr", 0, 0, invalid_read, system_write},
 #include "files.h"
 };
 
