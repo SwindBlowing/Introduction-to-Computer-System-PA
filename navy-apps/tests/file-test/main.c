549910599d7498e150b910277b03947277eb717b
tracer-ics2022 <tracer@njuics.org>
2022-10-22 17:23:43 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:23:43 up  2:32,  1 user,  load average: 0.39, 0.40, 0.44

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -20,7 +20,7 @@ int main() {
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
-
+  assert(0);
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
