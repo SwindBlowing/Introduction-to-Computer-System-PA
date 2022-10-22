d3eed744816e25761ae8dd9fbc4e84874a2f040b
tracer-ics2022 <tracer@njuics.org>
2022-10-22 16:58:01 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:58:01 up  2:06,  1 user,  load average: 0.59, 0.41, 0.32

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -25,6 +25,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   int fd = fs_open(filename, 0, 0);
   fs_lseek(fd, 0, SEEK_SET);
   fs_read(fd, &ehdr, sizeof(Elf_Ehdr));
+  printf("%x\n", ehdr.e_entry);
 
   //check part
   assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
