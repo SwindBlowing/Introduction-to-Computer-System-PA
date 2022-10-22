9f1bc5c20b8ef31ea90b1262363ada5e617f97f8
tracer-ics2022 <tracer@njuics.org>
2022-10-22 17:42:13 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:42:13 up  2:50,  1 user,  load average: 0.18, 0.35, 0.43

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -72,6 +72,7 @@ size_t fs_read(int fd, void *buf, size_t len)
 }
 size_t fs_write(int fd, const void *buf, size_t len)
 {
+	if (fd == 1 || fd == 2) return system_write(buf, open_offset[fd], len);
 	len = len < file_table[fd].disk_offset + file_table[fd].size - open_offset[fd] ?
 			len : file_table[fd].disk_offset + file_table[fd].size - open_offset[fd];
 	ramdisk_write(buf, open_offset[fd], len);
