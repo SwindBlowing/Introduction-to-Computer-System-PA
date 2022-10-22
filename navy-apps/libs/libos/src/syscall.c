7f7146eba78a7a1375458982a446cd4976c9aca5
tracer-ics2022 <tracer@njuics.org>
2022-10-22 13:05:51 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:05:51 up  1:42,  1 user,  load average: 0.17, 0.20, 0.23

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -62,7 +62,7 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count) {
   //_exit(SYS_write);
-  printf("%u\n", count);
+  printf("???%u???\n", count);
   return _syscall_(SYS_write, fd, (intptr_t)buf, count);
 }
 
