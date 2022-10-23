c2dfb1af01f6ee5fa8b31b2c3ec9146cfbfc7d2c
tracer-ics2022 <tracer@njuics.org>
2022-10-23 16:18:10 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:18:10 up  1:00,  1 user,  load average: 0.79, 0.57, 0.47

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -21,7 +21,6 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  printf("%u\n", len);
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if (ev.keycode == AM_KEY_NONE) return 0;
   size_t nowLen = 0;
@@ -41,7 +40,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 	nowLen++; if (nowLen == len) return nowLen;
   }
   *(char *)(buf + nowLen) = '\0'; nowLen++;
-  printf("%s\n", (char *)buf);
+  printf("%d %s\n", sizeof(keyname[ev.keycode]), (char *)buf);
   return nowLen;
 }
 
