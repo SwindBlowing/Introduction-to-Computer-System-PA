78883f39d34ebc2e0edee98e084bf91a030ede3f
tracer-ics2022 <tracer@njuics.org>
2022-10-23 10:39:52 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:39:52 up 38 min,  1 user,  load average: 0.35, 0.39, 0.36

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -7,12 +7,15 @@ int main()
 {
 	gettimeofday(&timeVal, NULL);
 	unsigned long now = timeVal.tv_usec;
+	printf("fuck!!!\n");
+	printf("%lu\n", now);
 	int j = 0;
 	while (1) {
 		j++;
 		if (j == 10000) {
 			j = 0;
 			gettimeofday(&timeVal, NULL);
+			printf("%lu\n", timeVal.tv_usec);
 			if (timeVal.tv_usec - now >= 500000) {
 				now = timeVal.tv_usec;
 				printf("0.5s passed!\n");
