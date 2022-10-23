61f4d31b21ed7ab4f257e454affb5336e4a438f7
tracer-ics2022 <tracer@njuics.org>
2022-10-23 16:03:46 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:03:46 up 46 min,  1 user,  load average: 0.32, 0.44, 0.37

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -47,6 +47,7 @@ void init_fs() {
 
 int fs_open(const char *pathname, int flags, int mode)
 {
+	Log("%s", pathname);
 	size_t nowSize = sizeof(file_table) / sizeof(Finfo);
 	for (int i = 0; i < nowSize; i++)
 		if (strcmp(pathname, file_table[i].name) == 0) {
