66c2bdcf7e813cd2623cd29b001eabf8f9dfe36e
tracer-ics2022 <tracer@njuics.org>
2022-11-14 19:28:15 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:28:15 up  1:05,  1 user,  load average: 0.27, 0.30, 0.24

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,5 +1,7 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <stdio.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
@@ -17,6 +19,21 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
+  char buf[100] = {'\0'};
+  char tmpBuf[100] = {'\0'};
+  while (1) {                             //can be better!!!!!!!!!!
+	if (NDL_PollEvent(buf, 50)) {
+		if (*(buf + 1) == 'd') event->type = SDL_KEYDOWN, event->key.type = SDL_KEYDOWN;
+		else if (*(buf + 1) == 'u') event->type = SDL_KEYUP, event->key.type = SDL_KEYUP;
+		strncpy(tmpBuf, buf + 3, 50);
+		for (int i = 0; i < sizeof(keyname) / sizeof(char *); i++)
+			if (strcmp(tmpBuf, keyname[i]) == 0) {
+				event->key.keysym.sym = i;
+				return 1;
+			}
+		return 0;
+  	}
+  }
   return 1;
 }
 
