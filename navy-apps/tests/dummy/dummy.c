723a83bb875ffaab45b162fe89d134af36fb4ff2
tracer-ics2022 <tracer@njuics.org>
2022-11-14 11:43:25 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:43:25 up  2:38,  1 user,  load average: 1.21, 0.64, 0.54

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -12,6 +12,6 @@ extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 int main() {
   //return _syscall_(SYS_yield, 0, 0, 0);
   fixedpt a = fixedpt_rconst(-1.7);
-  printf("%d", fixedpt_ceil(a));
+  printf("%d", fixedpt_floor(a));
   return 0;
 }
