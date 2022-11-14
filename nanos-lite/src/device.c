7bc73262f1075876608945f73a73c826e7f82bec
tracer-ics2022 <tracer@njuics.org>
2022-11-14 10:06:45 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:06:45 up  1:02,  1 user,  load average: 0.19, 0.40, 0.49

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -75,8 +75,8 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   //int sys_h = io_read(AM_GPU_CONFIG).height;
   int h = offset / sys_w;
   int w = offset % sys_w;
-  io_write(AM_GPU_FBDRAW, w, h, (uint32_t *)buf, len, 1, false);
-  //io_write(AM_GPU_FBDRAW, 0, 0, NULL, 0, 0, true);
+  io_write(AM_GPU_FBDRAW, w, h, (uint32_t *)buf, len, len, false);
+  io_write(AM_GPU_FBDRAW, 0, 0, NULL, 0, 0, true);
   return len;
 }
 
