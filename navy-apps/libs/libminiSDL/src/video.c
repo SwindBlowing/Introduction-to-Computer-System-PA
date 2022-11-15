8d9e49170d8d0cca16dc2469066c3e0fadb121bf
tracer-ics2022 <tracer@njuics.org>
2022-11-15 15:28:36 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:28:36 up  1:28,  1 user,  load average: 0.44, 0.62, 0.62

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -99,7 +99,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
 				uint8_t b = s->format->palette->colors[s->pixels[i * (s->w) + j]].b;
 				nowPixels[i * (s->w) + j] = ((uint32_t)r << 16) | ((uint32_t)g << 8) | (uint32_t)b;*/
 				nowPixels[i * (s->w) + j] = s->format->palette->colors[s->pixels[i * (s->w) + j]].val;
-				printf("0x%x\n", nowPixels[i * (s->w) + j]);
+				//printf("0x%x\n", nowPixels[i * (s->w) + j]);
 			}
 		printf("End4\n");
 		if (!x && !y && !w && !h) 
