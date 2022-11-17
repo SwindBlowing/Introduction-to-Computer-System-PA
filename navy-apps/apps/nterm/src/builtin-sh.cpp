0b6f723a7e6876a1a6adee913f526f7cecad85e6
tracer-ics2022 <tracer@njuics.org>
2022-11-17 10:54:06 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:54:06 up 34 min,  1 user,  load average: 0.78, 0.65, 0.67

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -26,10 +26,11 @@ static void sh_handle_cmd(const char *cmd) {
 	if (strcmp(cmd, "quit\n") == 0) exit(0);
 	if (cmd[0] == '.') {
 		static char tmp[99];
-		for (int i = 0; cmd[i]; i++) {
-			if (cmd[i] == '\n') tmp[i] = '\0';
-			else tmp[i] = cmd[i];
+		for (int i = 1; cmd[i]; i++) {
+			if (cmd[i] == '\n') tmp[i - 1] = '\0';
+			else tmp[i - 1] = cmd[i];
 		}
+		printf("%s\n", tmp);
 		execve(tmp, NULL, NULL);
 	}
 }
