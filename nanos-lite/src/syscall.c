0af201a0d2842c3307dbde0f39c654281ecb55cf
tracer-ics2022 <tracer@njuics.org>
2022-11-17 10:31:21 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:31:21 up 11 min,  1 user,  load average: 0.75, 0.97, 0.73

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,12 +1,15 @@
 #include <common.h>
 #include <fs.h>
 #include "syscall.h"
+#include <proc.h>
 
 //#define CONFIG_STRACE
 
 #ifdef CONFIG_STRACE
 static char nowFile[999] = {'\0'};
 #endif
+
+void naive_uload(PCB *pcb, const char *filename);
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
@@ -23,7 +26,7 @@ void do_syscall(Context *c) {
 #endif
 
   switch (a[0]) {
-	case SYS_exit: halt(a[1]); break;
+	case SYS_exit: naive_uload(NULL, "/bin/menu"); break;
 	case SYS_yield: yield(); c->GPRx = 0; break;
 	case SYS_open: c->GPRx = fs_open((const char *)a[1], a[2], a[3]); break;
 	case SYS_read: c->GPRx = fs_read(a[1], (void *)a[2], a[3]); break;
@@ -31,6 +34,7 @@ void do_syscall(Context *c) {
 	case SYS_close: c->GPRx = fs_close(a[1]); break;
 	case SYS_lseek: c->GPRx = fs_lseek(a[1], a[2], a[3]); break;
 	case SYS_brk: c->GPRx = 0; break;
+	case SYS_execve: naive_uload(NULL, (const char *)a[1]); break;
 	case SYS_gettimeofday: 
 		if ((void *)a[1] != NULL) {
 			*(size_t *)(a[1]) = io_read(AM_TIMER_UPTIME).us / 1000000u;
