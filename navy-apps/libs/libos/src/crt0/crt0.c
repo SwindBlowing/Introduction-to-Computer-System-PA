c6d931a51c131554f83f2b838adc498cc5cd66c0
tracer-ics2022 <tracer@njuics.org>
2022-12-23 16:38:44 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:38:44 up  1:31,  1 user,  load average: 0.62, 0.59, 0.46

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -6,9 +6,14 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  printf("%p\n", args);
-  char *empty[] =  {NULL };
-  environ = empty;
-  exit(main(0, empty, empty));
+  //printf("%p\n", args);
+  int argc = *args;
+  printf("%d\n", argc);
+  char *argv[argc];
+  char *envp[99];
+  //char *empty[] =  {NULL };
+  //environ = empty;
+  environ = envp;
+  exit(main(argc, argv, envp));
   assert(0);
 }
