6d77eabc902089c3874e6cb482657eb4775ac6dd
tracer-ics2022 <tracer@njuics.org>
2022-12-23 16:28:45 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:28:45 up  1:21,  1 user,  load average: 0.54, 0.34, 0.35

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -67,8 +67,8 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg)
 void context_uload(PCB *pcb, const char *filename)
 {
 	Area ustack;
-	ustack.end = heap.end; //top of the stack
-	ustack.start = heap.end - sizeof(pcb->stack);
+	ustack.start = pcb->stack;
+	ustack.end = ustack.start + sizeof(pcb->stack);
 	pcb->cp = ucontext(NULL, ustack, (void *)loader(pcb, filename));
-	pcb->cp->GPRx = (uintptr_t)ustack.end;
+	pcb->cp->GPRx = (uintptr_t)ustack.start;
 }
