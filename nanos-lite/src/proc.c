806235e2e331d12eff1de3dad1376da93bd57d4f
tracer-ics2022 <tracer@njuics.org>
2022-12-23 12:49:47 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:49:47 up  1:58,  1 user,  load average: 0.80, 0.66, 0.56

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -34,6 +34,8 @@ void init_proc() {
 
   context_kload(&pcb[0], hello_fun, NULL);
 
+  context_kload(&pcb[1], hello_fun, "The second Hello-World");
+
   switch_boot_pcb();
 
   Log("Initializing processes...");
