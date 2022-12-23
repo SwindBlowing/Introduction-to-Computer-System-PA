24bd280340ade4b89bccf7c99a3415de9d105bd8
tracer-ics2022 <tracer@njuics.org>
2022-12-23 13:28:58 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:28:58 up  2:37,  1 user,  load average: 0.43, 0.41, 0.36

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -70,11 +70,11 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   cp->mstatus = 0x1800;
 	printf("now %p\n", arg);
 
-  asm volatile("mv a0, %0" 
+  asm volatile("mv %0, a0" 
 		: 
 		: "g"((uint32_t)(((uintptr_t)arg >> 16)))
 		: "a0", "a1");
-		
+
   asm volatile("mv a1, %0" 
 		: 
 		: "g"((uint32_t)((uintptr_t)arg & 0x0000ffff))
