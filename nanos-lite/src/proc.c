216b83165f1c6ade553bae902a4de66ab9c540c1
tracer-ics2022 <tracer@njuics.org>
2022-12-23 12:51:49 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:51:49 up  2:00,  1 user,  load average: 0.46, 0.61, 0.55

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -52,7 +52,7 @@ Context* schedule(Context *prev) {
    // (before) always select pcb[0] as the new process
   //current = &pcb[0];
   // (current) transform between pcb[0] and pcb[1]
-  current = (current == &pcb[0]) ? &pcb[1] : &pcb[0];
+  current = ((current == &pcb[0]) ? &pcb[1] : &pcb[0]);
 
   // then return the new context
   return current->cp;
