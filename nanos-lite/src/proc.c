5d580251d074d46831f2baa6b7830d2bccd522c3
tracer-ics2022 <tracer@njuics.org>
2022-12-28 20:29:14 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:29:14 up  1:18,  1 user,  load average: 0.40, 0.37, 0.42

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -37,7 +37,7 @@ void init_proc() {
   static char *proc_empty[] = {NULL};
   /*static char *pal_argv[3] = {"/bin/pal", "--skip", NULL};
   context_uload(&pcb[1], "/bin/pal", pal_argv, proc_empty);*/
-  static char *proc_argvs[2] = {"/bin/pal", NULL};
+  static char *proc_argvs[3] = {"/bin/pal", "--skip", NULL};
   context_uload(&pcb[1], "/bin/pal", proc_argvs, proc_empty);
 
   switch_boot_pcb();
