896d707df89ddd27aff2701025c54c7a8cf5ba21
tracer-ics2022 <tracer@njuics.org>
2022-12-28 16:54:33 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:54:33 up  2:28,  1 user,  load average: 0.44, 0.35, 0.28

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -28,9 +28,9 @@ typedef uint32_t PTE;
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   //return MEM_RET_FAIL;
-  printf("vaddr:%x\n", vaddr);
+  printf("\nvaddr:%x\n", vaddr);
   paddr_t PTE_loc = satp_PPN * 4096 + VPN1(vaddr) * 4;
-  printf("\nPTE_loc:%x\n", PTE_loc);
+  printf("PTE_loc:%x\n", PTE_loc);
   PTE firstPTE = paddr_read(PTE_loc, sizeof(PTE));
   printf("firstPTE:%x\n", firstPTE);
   Assert(firstPTE & 0x1, "firstPTE %x is invalid", firstPTE);
