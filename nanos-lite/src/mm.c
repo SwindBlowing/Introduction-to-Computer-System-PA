5b21868b7bf0f2b4c3bf4ba648300b9a414f809a
tracer-ics2022 <tracer@njuics.org>
2022-12-28 20:21:58 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:21:58 up  1:11,  1 user,  load average: 0.22, 0.43, 0.48

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -26,20 +26,24 @@ void free_page(void *p) {
 extern PCB *current;
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk) {
-  uintptr_t max_pg_end = current->max_brk;
-  intptr_t max_pg = ((intptr_t)(max_pg_end >> 12)) - 1;
-  intptr_t brk_pg = (brk >> 12);
-  printf("%x %x\n", max_pg_end, brk);
-  //printf("Here\n");
-  if (brk_pg > max_pg) {
-	void *start = pg_alloc((brk_pg - max_pg) * PGSIZE);
-	for (int i = 0; i < brk_pg - max_pg; i++) {
-		//printf("%p %p\n", max_pg_end + i * PGSIZE, start + i * PGSIZE);
-		map(&current->as, (void *)(max_pg_end + i * PGSIZE),
-			start + i * PGSIZE, 3);
-	}
-	current->max_brk = (brk_pg + 1) * PGSIZE;
+  uintptr_t max_page_end = current->max_brk; 
+  uintptr_t max_page_pn = (max_page_end >> 12) - 1;
+  uintptr_t brk_pn = brk >> 12;//12
+
+  //[page_start, page_end) 所以，应该是大于等于就重新分配
+  //printf("申请内存 max_page_end: %p\t to brk: %p\n", max_page_end, brk);//分配从[max_page_pn, brk_pn]的所有页
+  if (brk >= max_page_end){
+    void *allocted_page =  new_page(brk_pn - max_page_pn + 1);//需要申请的新的页数
+    for (int i = 0; i < brk_pn - max_page_pn + 1; ++i){
+      //TODO: prot 有问题
+      map(&current->as, (void *)(max_page_end + i * 0xfff),
+       (void *)(allocted_page + i * 0xfff), 1);
+    }
+
+    current->max_brk = (brk_pn + 1) << 12;
+    assert(current->max_brk > brk);
   }
+
   return 0;
 }
 
