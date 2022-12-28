29af416534e207b7b76640b90b13d3439571f8b4
tracer-ics2022 <tracer@njuics.org>
2022-12-28 18:16:53 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:16:53 up  3:50,  1 user,  load average: 0.76, 0.54, 0.45

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -1,6 +1,7 @@
 #include <am.h>
 #include <nemu.h>
 #include <klib.h>
+#include <riscv/riscv.h>
 
 static AddrSpace kas = {};
 static void* (*pgalloc_usr)(int) = NULL;
@@ -29,6 +30,7 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
   pgfree_usr = pgfree_f;
 
   kas.ptr = pgalloc_f(PGSIZE);
+  printf("kas.ptr addr: %p\n", kas.ptr);
 
   int i;
   for (i = 0; i < LENGTH(segments); i ++) {
@@ -57,48 +59,59 @@ void unprotect(AddrSpace *as) {
 }
 
 void __am_get_cur_as(Context *c) {
-  c->pdir = (vme_enable ? (void *)get_satp() : NULL);
+  if (c->pdir != NULL){ //自行添加
+    //printf("在__am_get_cur_as中设置为由%p，地址为%p，更改为，", c->pdir, &c->pdir);
+    c->pdir = (vme_enable ? (void *)get_satp() : NULL);
+    //printf("%p\n", c->pdir);
+  }
 }
 
 void __am_switch(Context *c) {
   if (vme_enable && c->pdir != NULL) {
+    //printf("在__am_switch中设置satp为%p\n", c->pdir);
     set_satp(c->pdir);
   }
 }
 
-#define VPN0(x) (((uintptr_t)(x) & 0x003ff000) >> 12)
-#define VPN1(x) (((uintptr_t)(x) & 0xffc00000) >> 22)
-#define PTE_PPN(x) ((uintptr_t)(x) >> 10)
-#define LOC_PPN(x) ((uintptr_t)(x) >> 12)
+#define VA_VPN_0(x) (((uintptr_t)x & 0x003FF000u) >> 12)
+#define VA_VPN_1(x) (((uintptr_t)x & 0xFFC00000u) >> 22)
+#define VA_OFFSET(x) ((uintptr_t)x & 0x00000FFFu)
+
+#define PTE_PPN_MASK (0xFFFFFC00u)
+#define PTE_PPN(x) (((uintptr_t)x & PTE_PPN_MASK) >> 10)
 
 void map(AddrSpace *as, void *va, void *pa, int prot) {
-	va = (void *)((uintptr_t)(va) & (~0xfff));
-	pa = (void *)((uintptr_t)(pa) & (~0xfff));
-	//printf("%p <=> %p\n", va, pa);
-	//printf("@@@%d\n", sizeof(PTE));
-	//if ((uintptr_t)(va) == 0x80001000) printf("pa here:%x\n", (uintptr_t)(pa));
+  va = (void *)(((uintptr_t)va) & (~0xfff));
+  pa = (void *)(((uintptr_t)pa) & (~0xfff));
 
-	PTE *PT_entry = as->ptr + VPN1(va) * 4;
-	if (!(*PT_entry & 0x1)) {
-		void *new_leaf_page = pgalloc_usr(PGSIZE);
-		//printf("newpage:%p\n", new_leaf_page);
-		*PT_entry = (*PT_entry & 0x3ff) | (((uintptr_t)(new_leaf_page) >> 12) << 10);
-		*PT_entry = (*PT_entry | 0x1);
-		//printf("%x\n", *PT_entry);
-	}
-	//printf("%x\n", PPN(*PT_entry));
-	//printf("%x\n", PPN(*PT_entry) * 4096 + VPN0(va) * 4);
-	PTE *leaf_PTE = (PTE *)(PTE_PPN(*PT_entry) * 4096 + VPN0(va) * 4);
-	*leaf_PTE = (LOC_PPN(pa) << 10) | 0xf;
-	//if ((uintptr_t)(va) == 0x80001000) printf("leafPTE here:%x\n", *leaf_PTE);
+  PTE *page_table_entry = as->ptr + VA_VPN_1(va) * 4;
+  // assert((uintptr_t)as->ptr + VA_VPN_1(va) * 4 == get_satp() + VA_VPN_1(va) * 4);
+
+  if (!(*page_table_entry & PTE_V)){ // 说明二级表未分配
+    void *alloced_page = pgalloc_usr(PGSIZE);
+    *page_table_entry = (*page_table_entry & ~PTE_PPN_MASK) | (PTE_PPN_MASK & ((uintptr_t)alloced_page >> 2));
+    *page_table_entry = (*page_table_entry | PTE_V);
+    // printf("二级表未分配\t二级表项地址:%p\t虚拟地址:%p\n", page_table_entry, va);
+    //assert(((PTE_PPN(*page_table_entry) * 4096 + VA_VPN_0(va) * 4) & ~0xFFFFFF) == ((uintptr_t)alloced_page& ~0xFFFFFF));
+  }
+  // 找到二级表中的表项
+  PTE *leaf_page_table_entry = (PTE *)(PTE_PPN(*page_table_entry) * 4096 + VA_VPN_0(va) * 4);
+  // if ((uintptr_t)va <= 0x40100000){
+  //   printf("设置二级表项\t虚拟地址:%p\t实际地址:%p\t表项:%p\n", va, pa, leaf_page_table_entry);
+  // }
+  // 设置PPN
+  *leaf_page_table_entry = (PTE_PPN_MASK & ((uintptr_t)pa >> 2)) | (PTE_V | PTE_R | PTE_W | PTE_X) | (prot ? PTE_U : 0);
+  //assert(PTE_PPN(*leaf_page_table_entry) * 4096 + VA_OFFSET(va) == (uintptr_t)pa);
 }
 
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
+  Context *context = kstack.end - sizeof(Context);
+  context->mstatus = 0xC0000 | 0x80;//MPP设置为U模式，MXR=1，SUM=1
+  context->mepc    = (uintptr_t)entry;
+  context->pdir    = as->ptr;
+  //为了real VME
+  //context->np      = 0;//USER
+  //在loader中设置sp
 
-  Context *cp = (Context *)kstack.end - 1;
-  cp->mepc = (uintptr_t)entry;
-  cp->mstatus = 0x1800;
-  cp->pdir = as->ptr;
-
-  return cp;
+  return context;
 }
