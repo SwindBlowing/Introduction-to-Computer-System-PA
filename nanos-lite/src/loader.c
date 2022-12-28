1014f1256a8fc847e0ed5515f61968fb9c48c6f0
tracer-ics2022 <tracer@njuics.org>
2022-12-28 17:48:27 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:48:27 up  3:22,  1 user,  load average: 0.47, 0.29, 0.28

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -116,7 +116,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 	//create the String area
 
 	uintptr_t stack_argv[argc], stack_envp[sz_envp];
-	char *now = (char *)(ustack.end - 4);
+	char *now = (char *)(pcb->cp - 4);
 	*now = 0;
 	for (int j = sz_envp - 1; j >= 0; j--) {
 		now -= strlen(envp[j]);
