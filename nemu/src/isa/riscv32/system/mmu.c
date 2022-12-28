794f495e8e1ab36af91db3d9602dc9aaf4574e4e
tracer-ics2022 <tracer@njuics.org>
2022-12-28 19:59:13 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:59:13 up 48 min,  1 user,  load average: 0.59, 0.52, 0.37

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -1,43 +1,52 @@
+/***************************************************************************************
+* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
 #include <isa.h>
-#include <isa-def.h>
 #include <memory/paddr.h>
 #include <memory/vaddr.h>
+#include <cpu/cpu.h>
 
-#define VA_VPN_0(x) (((vaddr_t)x & 0x003FF000u) >> 12)
-#define VA_VPN_1(x) (((vaddr_t)x & 0xFFC00000u) >> 22)
-#define VA_OFFSET(x) ((vaddr_t)x & 0x00000FFFu)
-
-#define PTE_PPN_MASK (0xFFFFFC00u)
-#define PTE_PPN(x) (((vaddr_t)x & PTE_PPN_MASK) >> 10)
-
-#define PTE_V 0x01
-#define PTE_A 0x40
-#define PTE_D 0x80
+#define VPN0(x) (((vaddr_t)(x) & 0x003ff000) >> 12)
+#define VPN1(x) (((vaddr_t)(x) & 0xffc00000) >> 22)
+#define PTE_PPN(x) ((vaddr_t)(x) >> 10)
+#define satp_PPN (cpu.satp & 0x3fffff)
+#define offset(x) ((vaddr_t)(x) & 0xfff)
 
 typedef uint32_t PTE;
-extern CPU_state cpu;
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-  paddr_t page_table_entry_addr = (cpu.satp << 12) + VA_VPN_1(vaddr) * 4;
-  PTE page_table_entry = paddr_read(page_table_entry_addr, 4);
-  // if ((page_table_entry & PTE_V) == 0){
-  //   Log("page_table_entry not valid, vaddr: %#x", vaddr);
-  // }
-  Assert(page_table_entry & PTE_V, "page_table_entry not valid, vaddr: %#x", vaddr);
+  //return MEM_RET_FAIL;
+  //printf("\nvaddr:%x\n", vaddr);
+  paddr_t PTE_loc = satp_PPN * 4096 + VPN1(vaddr) * 4;
+  //printf("PTE_loc:%x\n", PTE_loc);
+  PTE firstPTE = paddr_read(PTE_loc, sizeof(PTE));
+  //printf("firstPTE:%x\n", firstPTE);
+  Assert(firstPTE & 0x1, "firstPTE %x with vaddr %x is invalid", firstPTE, vaddr);
 
-  paddr_t leaf_page_table_entry_addr = PTE_PPN(page_table_entry) * 4096 + VA_VPN_0(vaddr) * 4;
-  PTE leaf_page_table_entry = paddr_read(leaf_page_table_entry_addr, 4);
-  // if ((leaf_page_table_entry & PTE_V) == 0){
-  //   Log("leaf_page_table_entry not valid, vaddr: %#x, pte_entry: %#x", vaddr, leaf_page_table_entry_addr);
-  // }
-  Assert(leaf_page_table_entry & PTE_V, "leaf_page_table_entry not valid, vaddr: %#x, pte_entry: %#x", vaddr, leaf_page_table_entry_addr);
-  if (type == 0){//读
-    paddr_write(leaf_page_table_entry_addr, 4, leaf_page_table_entry | PTE_A);
-  }else if (type == 1){//写
-    paddr_write(leaf_page_table_entry_addr, 4, leaf_page_table_entry | PTE_D);
-  }
-  paddr_t pa = PTE_PPN(leaf_page_table_entry) * 4096 + VA_OFFSET(vaddr);
-  // assert(pa == vaddr);
+  paddr_t leaf_PTE_loc = PTE_PPN(firstPTE) * 4096 + VPN0(vaddr) * 4;
+  //printf("leaf_PTE_loc:%x\n", leaf_PTE_loc);
+  PTE leafPTE = paddr_read(leaf_PTE_loc, sizeof(PTE));
+  Assert(leafPTE & 0x1, "leafPTE %x with vaddr %x is invalid", leafPTE, vaddr);
+  
+  paddr_write(leaf_PTE_loc, sizeof(PTE), leafPTE | (1ul << 6));
+  if (type == 1) 
+  	paddr_write(leaf_PTE_loc, sizeof(PTE), leafPTE | (1ul << 7));
 
-  return pa;
+  paddr_t paddr = PTE_PPN(leafPTE) * 4096 + offset(vaddr);
+  //printf("%x %x\n", vaddr, paddr);
+  //Assert(paddr == vaddr, "Incorrect translation with va = %x and pa = %x", vaddr, paddr);
+	//assert(0);
+  return paddr;
 }
