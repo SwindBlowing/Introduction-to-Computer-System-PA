142e2e5c958064f70bee628a792cbaff56ea3a58
tracer-ics2022 <tracer@njuics.org>
2022-12-28 13:22:55 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:22:55 up  1:46,  1 user,  load average: 0.52, 0.43, 0.33

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -75,6 +75,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 	pa = (void *)((uintptr_t)(pa) & (~0xfff));
 
 	PTE *PT_entry = as->ptr + VPN1(va) * 4;
+	printf("%x\n", *PT_entry);
 	if (!(*PT_entry & 0x1)) {
 		void *new_leaf_page = pgalloc_usr(PGSIZE);
 		printf("newpage:%p\n", new_leaf_page);
