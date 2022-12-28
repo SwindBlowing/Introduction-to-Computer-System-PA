836c3ab55f8f191f2388ffc9f51e42f9106f2114
tracer-ics2022 <tracer@njuics.org>
2022-12-28 13:33:03 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:33:03 up  1:56,  1 user,  load average: 0.52, 0.27, 0.27

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -77,13 +77,13 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 	PTE *PT_entry = as->ptr + VPN1(va) * 4;
 	if (!(*PT_entry & 0x1)) {
 		void *new_leaf_page = pgalloc_usr(PGSIZE);
-		printf("newpage:%p\n", new_leaf_page);
+		//printf("newpage:%p\n", new_leaf_page);
 		*PT_entry = (*PT_entry & 0x3ff) | (((uintptr_t)(new_leaf_page) >> 12) << 10);
 		*PT_entry = (*PT_entry | 0x1);
-		printf("%x\n", *PT_entry);
+		//printf("%x\n", *PT_entry);
 	}
-	printf("%x\n", PPN(*PT_entry));
-	printf("%x\n", PPN(*PT_entry) * 4096 + VPN0(va) * 4);
+	//printf("%x\n", PPN(*PT_entry));
+	//printf("%x\n", PPN(*PT_entry) * 4096 + VPN0(va) * 4);
 	PTE *leaf_PTE = (PTE *)(PPN(*PT_entry) * 4096 + VPN0(va) * 4);
 	*leaf_PTE = (0xfffffc00 & PPN(pa)) | 0xf;
 }
