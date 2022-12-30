2f14a193b94271e2ff59671a4fc8e7621746c145
tracer-ics2022 <tracer@njuics.org>
2022-12-30 22:09:31 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:09:31 up  3:31,  1 user,  load average: 0.25, 0.40, 0.40

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -151,6 +151,6 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 	}
 	p--; *p = argc;
 
-	pcb->cp->GPRx = (uintptr_t)p;
+	pcb->cp->GPRx = (uintptr_t)((void *)p - ustack.end + pcb->as.area.end);
 	pcb->cp->gpr[2] = (uintptr_t)p;
 }
