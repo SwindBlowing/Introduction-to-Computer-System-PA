93976e98f85925efa86aa7b7bd54ee8e0380e1c2
tracer-ics2022 <tracer@njuics.org>
2022-12-30 23:22:54 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:22:54 up  4:44,  1 user,  load average: 0.48, 0.50, 0.52

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -26,14 +26,14 @@ void free_page(void *p) {
 extern PCB *current;
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk) {
-	printf("%x %x\n", brk, current->max_brk);
+	//printf("%x %x\n", brk, current->max_brk);
   uintptr_t max_pg_end = current->max_brk; // must ensure that max_brk = n * PGSIZE
   intptr_t max_pg = ((intptr_t)(max_pg_end >> 12)) - 1; // can be "-1", and max_pg_end must be the multiply of PGSIZE to make it correct
   intptr_t brk_pg = (brk >> 12);
   //printf("%x %x\n", max_pg_end, brk);
   //printf("Here\n");
   if (brk_pg > max_pg) {
-	printf("Here\n");
+	//printf("Here\n");
 	void *start = pg_alloc((brk_pg - max_pg) * PGSIZE);
 	for (int i = 0; i < brk_pg - max_pg; i++) {
 		//printf("%p %p\n", max_pg_end + i * PGSIZE, start + i * PGSIZE);
