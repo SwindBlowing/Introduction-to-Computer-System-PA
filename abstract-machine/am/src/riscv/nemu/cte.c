5b357cc1364d315ff679061ecf9ca6c83cba0776
tracer-ics2022 <tracer@njuics.org>
2022-12-30 21:54:41 +0800

    >  compile NEMU 211850008 林朗 Linux swind-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:54:41 up  3:16,  1 user,  load average: 0.47, 0.30, 0.27

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -91,7 +91,6 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 
 void yield() {
   asm volatile("li a7, -1; ecall");
-  printf("Here\n");
 }
 
 bool ienabled() {
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -37,8 +37,8 @@ void init_proc() {
   static char *proc_empty[] = {NULL};
   /*static char *pal_argv[3] = {"/bin/pal", "--skip", NULL};
   context_uload(&pcb[1], "/bin/pal", pal_argv, proc_empty);*/
-  static char *proc_argvs[3] = {"/bin/dummy", NULL};
-  context_uload(&pcb[1], "/bin/dummy", proc_argvs, proc_empty);
+  static char *proc_argvs[3] = {"/bin/hello", NULL};
+  context_uload(&pcb[1], "/bin/hello", proc_argvs, proc_empty);
 
   switch_boot_pcb();
 
